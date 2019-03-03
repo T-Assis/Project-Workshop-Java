8e81b80e3f59544ae171ef8432e142f13efe7844
Thales Lima de Assis <thaleslima-19@hotmail.com>
2019-03-03 11:01:44 -0300

    Project created

---
+++ src/main/java/br/com/projetcworkshop/ProjectWorkshopJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetcworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWorkshopJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWorkshopJavaApplication.class, args);
+	}
+
+}
