acf7229a7d13220869b7d4dd2403c03bedad6a25
Thales Lima de Assis <thaleslima-19@hotmail.com>
2019-03-17 10:33:00 -0300

    Custom Validation: Email not repeated in Cliente update

--- src/main/java/br/com/projetcworkshop/dto/ClienteDTO.java
+++ src/main/java/br/com/projetcworkshop/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.projetcworkshop.domain.Cliente;
+import br.com.projetcworkshop.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
