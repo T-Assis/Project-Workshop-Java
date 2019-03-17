7750104064f97fc8299dcb91a25ee5f766d7dee7
Thales Lima de Assis <thaleslima-19@hotmail.com>
2019-03-17 10:15:09 -0300

    Custom Validation: Email not repeated when inserted a new Cliente

--- src/main/java/br/com/projetcworkshop/repositories/ClienteRepository.java
+++ src/main/java/br/com/projetcworkshop/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package br.com.projetcworkshop.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.projetcworkshop.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
+	
 }
--- src/main/java/br/com/projetcworkshop/services/validation/ClienteInsertValidator.java
+++ src/main/java/br/com/projetcworkshop/services/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.projetcworkshop.domain.Cliente;
 import br.com.projetcworkshop.domain.enums.TipoCliente;
 import br.com.projetcworkshop.dto.ClienteNewDTO;
+import br.com.projetcworkshop.repositories.ClienteRepository;
 import br.com.projetcworkshop.resources.exception.FieldMessage;
 import br.com.projetcworkshop.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
-
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -29,7 +36,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (clienteNewDto.getTipoCliente().equals(TipoCliente.PESSOA_JURIDICA.getCodigo()) && !BR.isValidCNPJ(clienteNewDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
-
+		
+		Cliente cliente = clienteRepository.findByEmail(clienteNewDto.getEmail());
+		if (cliente != null) {
+			list.add(new FieldMessage("email", "E-mail já existente"));
+		}
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
