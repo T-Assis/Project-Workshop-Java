fb9bdb6620968e91202d49bd63e4028c012308dd
Thales Lima de Assis <thaleslima-19@hotmail.com>
2019-06-21 17:15:27 -0300

    Content Restriction: Client only retrieves your Order

--- src/main/java/br/com/projetcworkshop/resources/PedidoResource.java
+++ src/main/java/br/com/projetcworkshop/resources/PedidoResource.java
@@ -6,12 +6,14 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -25,16 +27,19 @@ public class PedidoResource {
 	@Autowired
 	private PedidoService service;
 
-	@GetMapping
-	public ResponseEntity<List<Pedido>> findAll() {
-		List<Pedido> list = service.findAll();
-		return ResponseEntity.ok().body(list);
-	}
-	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Pedido> findById(@PathVariable Integer id) {
 		Pedido categoria = service.findById(id);
 		return ResponseEntity.ok().body(categoria);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<Pedido>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page, 
+			@RequestParam(value = "linesPerPage", defaultValue = "24")  Integer linesPerPage, 
+			@RequestParam(value = "orderBy", defaultValue = "instante")  String orderBy, 
+			@RequestParam(value = "direction", defaultValue = "DESC")  String direction) {
+		Page<Pedido> list = service.findPage(page, linesPerPage, orderBy, direction);
+		return ResponseEntity.ok().body(list);
 	}
 	
 	@PostMapping
--- src/main/java/br/com/projetcworkshop/services/PedidoService.java
+++ src/main/java/br/com/projetcworkshop/services/PedidoService.java
@@ -5,9 +5,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import br.com.projetcworkshop.domain.Cliente;
 import br.com.projetcworkshop.domain.ItemPedido;
 import br.com.projetcworkshop.domain.PagamentoBoleto;
 import br.com.projetcworkshop.domain.Pedido;
@@ -15,6 +19,8 @@ import br.com.projetcworkshop.domain.enums.StatusPagamento;
 import br.com.projetcworkshop.repositories.ItemPedidoRepository;
 import br.com.projetcworkshop.repositories.PagamentoRepository;
 import br.com.projetcworkshop.repositories.PedidoRepository;
+import br.com.projetcworkshop.security.UserSpringSecurity;
+import br.com.projetcworkshop.services.exception.AuthorizationException;
 import br.com.projetcworkshop.services.exception.ObjectNotFoundException;
 
 @Service
@@ -75,6 +81,16 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(pedido.getItens());
 		emailService.sendOrderConfirmationEmail(pedido);
 		return pedido;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		UserSpringSecurity userSS = UserService.authenticated();
+		if (userSS == null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente =  clienteService.findById(userSS.getId());
+		return repository.findByCliente(cliente, pageRequest);
 	}
 	
 
