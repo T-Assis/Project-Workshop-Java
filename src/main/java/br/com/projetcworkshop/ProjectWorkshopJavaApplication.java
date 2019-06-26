955b7195d730a55f18bad79ea9041da2f4081496
Thales Lima de Assis <thaleslima-19@hotmail.com>
2019-06-26 15:31:48 -0300

    Saving the first file in Amazon S3

--- src/main/java/br/com/projetcworkshop/ProjectWorkshopJavaApplication.java
+++ src/main/java/br/com/projetcworkshop/ProjectWorkshopJavaApplication.java
@@ -1,11 +1,17 @@
 package br.com.projetcworkshop;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.projetcworkshop.services.S3Service;
+
 @SpringBootApplication
 public class ProjectWorkshopJavaApplication implements CommandLineRunner {
+
+	@Autowired
+	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectWorkshopJavaApplication.class, args);
@@ -13,7 +19,7 @@ public class ProjectWorkshopJavaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\Users\\thales.lima.de.assis\\Pictures\\imagemUniverso.jpg");
 	}
 
 }
