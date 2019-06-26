a94a32ff96540419d948342e61ed3547d0478178
Thales Lima de Assis <thaleslima-19@hotmail.com>
2019-06-26 16:40:41 -0300

    Image upload to Amazon S3 through endpoint

--- src/main/java/br/com/projetcworkshop/services/ClienteService.java
+++ src/main/java/br/com/projetcworkshop/services/ClienteService.java
@@ -1,5 +1,6 @@
 package br.com.projetcworkshop.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.projetcworkshop.domain.Cidade;
 import br.com.projetcworkshop.domain.Cliente;
@@ -38,6 +40,9 @@ public class ClienteService {
 	
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public List<Cliente> findAll() {
 		return repository.findAll();
@@ -109,6 +114,10 @@ public class ClienteService {
 		newCliente.setNome(cliente.getNome());
 		newCliente.setEmail(cliente.getEmail());
 
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 }
--- src/main/java/br/com/projetcworkshop/services/S3Service.java
+++ src/main/java/br/com/projetcworkshop/services/S3Service.java
@@ -1,17 +1,19 @@
 package br.com.projetcworkshop.services;
 
-import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.AmazonClientException;
-import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 
 @Service
 public class S3Service {
@@ -24,20 +26,29 @@ public class S3Service {
 	@Value("${s3.bucket}")
 	private String bucketName;
 
-	public void uploadFile(String localFilePath) {
+	public URI uploadFile(MultipartFile multipartFile) {
 		try {
-			File file = new File(localFilePath);
-			LOG.info("Iniciando upload para Amazon S3... ");
-			s3Client.putObject(new PutObjectRequest(bucketName, "TesteNomeArquivo.jpg", file));
-			LOG.info("Upload realizado com sucesso.");
-		} 
-		catch (AmazonServiceException e) {
-			LOG.info("AmazonServiceException: " + e.getMessage());
-			LOG.info("Status Code: " + e.getErrorCode());
-		} 
-		catch (AmazonClientException e) {
-			LOG.info("AmazonClientException: " + e.getMessage());
+			String fileName = multipartFile.getOriginalFilename();
+			InputStream inputStream = multipartFile.getInputStream();
+			String contentType = multipartFile.getContentType();
+			return uploadFile(inputStream, fileName, contentType);
 		}
-
+		catch (IOException e) {
+			throw new RuntimeException("Erro de IO: " + e.getMessage());
+		}
+		
+	}
+	
+	public URI uploadFile(InputStream inputStream, String fileName, String contentType) {
+		try {
+			ObjectMetadata objectMetadata = new ObjectMetadata();
+			objectMetadata.setContentType(contentType);
+			LOG.info("Iniciando upload para Amazon S3... ");
+			s3Client.putObject(bucketName, fileName, inputStream, objectMetadata);
+			LOG.info("Upload realizado com sucesso.");
+			return s3Client.getUrl(bucketName, fileName).toURI();
+		} catch (URISyntaxException e) {
+			throw new RuntimeException("Erro ao converter URL para URI.");
+		}
 	}
 }
