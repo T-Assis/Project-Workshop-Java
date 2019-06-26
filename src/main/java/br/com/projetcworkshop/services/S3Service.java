1916935dbac267b47c874907b1bf61fa216b8759
Thales Lima de Assis <thaleslima-19@hotmail.com>
2019-06-26 17:03:44 -0300

    Handling Exceptions Properly

--- src/main/java/br/com/projetcworkshop/services/S3Service.java
+++ src/main/java/br/com/projetcworkshop/services/S3Service.java
@@ -15,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+import br.com.projetcworkshop.services.exception.FileException;
+
 @Service
 public class S3Service {
 
@@ -34,7 +36,7 @@ public class S3Service {
 			return uploadFile(inputStream, fileName, contentType);
 		}
 		catch (IOException e) {
-			throw new RuntimeException("Erro de IO: " + e.getMessage());
+			throw new FileException("Erro de IO: " + e.getMessage());
 		}
 		
 	}
@@ -48,7 +50,7 @@ public class S3Service {
 			LOG.info("Upload realizado com sucesso.");
 			return s3Client.getUrl(bucketName, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI.");
+			throw new FileException("Erro ao converter URL para URI.");
 		}
 	}
 }
